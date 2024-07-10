73187e3614a2b1b33e6f48b246fee1716e76ab2a
ratamranjith@gmail.com <ratamranjith@gmail.com>
2024-07-10 22:01:59 +0530

    Quick Sort using Divide and Conquer Method

---
+++ Sort/QuickSort.py
@@ -0,0 +1,22 @@
+'''
+Quick Sort:
+- Divide and Conquer Algorithm
+- It is a recursive algorithm that consists of two steps:
+- Partitioning: It is used to partition the array around an element called pivot.
+- Sorting: Recursively sort the subarrays.
+'''
+
+def quickSort(arrayValue):
+    
+    if(len(arrayValue) <= 1):
+        return arrayValue
+
+    pivot = arrayValue[len(arrayValue)//2] # Divide and Conquer
+    left = [i for i in arrayValue if(i < pivot)] # Pivot with left comparison
+    right = [i for i in arrayValue if(i > pivot)] # Pivot with left comparison
+    middle = [i for i in arrayValue if(i == pivot)] # Pivot with middle comparison
+    
+    return (quickSort(left) + middle + quickSort(right))
+
+data = [1, 7, 4, 1, 10, 9, -2]
+print(quickSort(data))
