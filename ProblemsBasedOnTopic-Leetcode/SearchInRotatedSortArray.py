ab3e7f81f7a51d99e784d70b3f92a38b70a4db2e
ratamranjith@gmail.com <ratamranjith@gmail.com>
2024-07-10 23:05:54 +0530

    Applying the linear search

--- ProblemsBasedOnTopic-Leetcode/SearchInRotatedSortArray.py
+++ ProblemsBasedOnTopic-Leetcode/SearchInRotatedSortArray.py
@@ -34,21 +34,12 @@ def search(nums, target):
     if(len(nums) < 1):
         return -1
     
-    start = 0
-    end   = len(nums) - 1
-    
-    # Applying Binary Search
-    while(start <= end):
-        mid = (start + end)//2
-        if(nums[mid] == target):
-            return mid
-        elif(target < nums[mid]):
-            end = mid - 1
-        else:
-            start  = mid + 1
+    for i in range(len(nums)):
+        if(nums[i] == target):
+            return i
     else:
         return -1
 
 nums = [4,5,6,7,0,1,2]
-target = 7
+target = 0
 print(search(nums, target))
