5a538c9a54b0c71714f302b7d69b245ca2ca596e
ratamranjith@gmail.com <ratamranjith@gmail.com>
2024-07-13 20:32:54 +0530

    Stack using list

---
+++ Stack/Stack-usingList.py
@@ -0,0 +1,16 @@
+stack = [1,2,3,4,5,6,7,8]
+
+# Insertion 
+stack.append(9) # will insert at the end time & space : O(1)
+print(stack)
+
+# Access
+print(stack[-1]) # will access the last element based on the negative index and time & space : O(1)
+print(stack[2]) # will access the indexed value in between time & space : 0(n)
+
+# Search
+print(9 in stack) # will search the element in the stack and time & space : O(n)
+
+# Delete
+stack.pop() # will delete the last element in the stack and time & space : O(1)
+print(stack)
