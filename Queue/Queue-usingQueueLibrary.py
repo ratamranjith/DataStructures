132ecfa31fdad6435a145169e4651d557386a32d
ratamranjith@gmail.com <ratamranjith@gmail.com>
2024-07-18 01:22:21 +0530

    adding priority queue package to the import

--- Queue/Queue-usingQueueLibrary.py
+++ Queue/Queue-usingQueueLibrary.py
@@ -1,4 +1,4 @@
-from queue import SimpleQueue, Queue
+from queue import SimpleQueue, Queue, PriorityQueue
 
 from collections import deque
 
@@ -65,3 +65,23 @@ print(deque)
 # Deletion
 print(deque.pop()) # Removes from backside
 print(deque.popleft()) # Removes from frontside
+
+#------------------
+# 4. Priority Queue
+#------------------
+priQue = PriorityQueue()
+
+# Insertion
+priQue.put(5)
+priQue.put(5)
+priQue.put(5)
+priQue.put_nowait(10) # Most Priority has been moved to index 0
+priQue.put(0)
+priQue.put(2)
+priQue.put(1)
+priQue.put(5)
+print(priQue.queue) # [0, 5, 1, 5, 5, 5, 2, 10]
+
+#deletion
+print(priQue.get_nowait()) # 0 : Least one will be removed
+print(priQue.queue) # [1, 5, 2, 5, 5, 5, 10]
