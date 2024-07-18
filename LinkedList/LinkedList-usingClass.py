654106c4525052490ff361cc7689cc8830ba7be4
ratamranjith@gmail.com <ratamranjith@gmail.com>
2024-07-18 22:53:03 +0530

    Linked using class

---
+++ LinkedList/LinkedList-usingClass.py
@@ -0,0 +1,24 @@
+'''
+Linked List - Using Class (Straight forward Approach)
+'''
+
+class Node:
+    
+    def __init__(self, data):
+        self.data = data
+        self.next = None # Pointer to the address
+
+
+
+head = Node(1) 
+node2 = Node(2)
+node3 = Node(3)
+
+head.next = node2
+node2.next = node3
+
+current = head # Temporary variable without affecting the head node
+
+while(current != None):
+    print(current.data)
+    current = current.next # Next Node address is being pointed
