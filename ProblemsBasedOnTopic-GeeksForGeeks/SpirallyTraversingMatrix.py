f6d74c01a3f9fea60188d9eb7e554a1083ab1b3c
ratamranjith@gmail.com <ratamranjith@gmail.com>
2024-08-01 22:59:34 +0530

    Interesting matrix problem

---
+++ ProblemsBasedOnTopic-GeeksForGeeks/SpirallyTraversingMatrix.py
@@ -0,0 +1,52 @@
+"""
+Spirally traversing a matrix
+Difficulty: MediumAccuracy: 35.2%Submissions: 266K+Points: 4
+You are given a rectangular matrix, and your task is to return an array while traversing the matrix in spiral form.
+
+Examples:
+
+Input: matrix[][] = [[1, 2, 3, 4],
+                  [5, 6, 7, 8],
+                  [9, 10, 11, 12],
+                  [13, 14, 15,16]]
+Output: [1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10]
+Explanation:
+
+Input: matrix[][] = [[1, 2, 3, 4],
+                  [5, 6, 7, 8],
+                  [9, 10, 11, 12]]
+Output: [1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7]
+Explanation: Applying same technique as shown above, output for the 2nd testcase will be 1 2 3 4 8 12 11 10 9 5 6 7.
+Expected Time Complexity: O(n2)
+Expected Auxiliary Space: O(n2)
+
+Constraints:
+1 <= matrix.size(), matrix[0].size() <= 100
+0 <= matrix[i][j]<= 100
+"""
+
+matrix = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
+
+
+def spirallyTraverse(matrix):
+    result = []
+    left, right = 0, len(matrix[0]) - 1
+    top, bottom = 0, len(matrix) - 1
+
+    while left <= right and top <= bottom:
+        for i in range(left, right + 1):
+            result.append(matrix[top][i])
+        top += 1
+        for i in range(top, bottom + 1):
+            result.append(matrix[i][right])
+        right -= 1
+        for i in range(right, left - 1, -1):
+            result.append(matrix[bottom][i])
+        bottom -= 1
+        for i in range(bottom, top - 1, -1):
+            result.append(matrix[i][left])
+        left += 1
+    return result
+
+
+print(spirallyTraverse(matrix))
